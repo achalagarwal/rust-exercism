031c870b75d2bb7a88072e39823e2e0b61d3fd93
Achal Agarwal <achalagarwal.01@gmail.com>
2019-10-24 08:11:27 +0200

    This is the non idiomatic way to compute the reverse of a string

--- reverse-string/src/lib.rs
+++ reverse-string/src/lib.rs
@@ -1,15 +1,15 @@
 pub fn reverse(input: &str) -> String {
 
-    // let len = input.len();
-    // let mut string = String::with_capacity(len);
-    // let mut iter = input.chars().rev();
-    // iter.for_each(|c| string.push(c));
-    // return string;
+    let len = input.len();
+    let mut string = String::with_capacity(len);
+    let mut iter = input.chars().rev();
+    iter.for_each(|c| string.push(c));
+    return string;
 
-    input.chars().rev().fold(String::with_capacity(input.len()), |mut string, c| {
-        string.push(c);
-        string
-        })
+    // input.chars().rev().fold(String::with_capacity(input.len()), |mut string, c| {
+    //     string.push(c);
+    //     string
+    //     })
     // unimplemented!("Write a function to reverse {}", input);
 
     
