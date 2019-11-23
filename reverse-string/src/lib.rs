07d48b1f30eee0b495bf90d8fb89e399b60ca3fa
Achal Agarwal <achalagarwal.01@gmail.com>
2019-11-23 16:38:12 +0100

    Modified Reverse String Src as required

--- reverse-string/src/lib.rs
+++ reverse-string/src/lib.rs
@@ -1,16 +1,29 @@
 pub fn reverse(input: &str) -> String {
 
+    /*
+     * Approach 1: Non functional, non idiomatic
+     */
+
     // let len = input.len();
     // let mut string = String::with_capacity(len);
     // let mut iter = input.chars().rev();
     // iter.for_each(|c| string.push(c));
     // return string;
 
-    input.chars().rev().fold(String::with_capacity(input.len()), |mut string, c| {
-        string.push(c);
-        string
-        })
-    // unimplemented!("Write a function to reverse {}", input);
+    /*
+     * Approach 2: functional, idiomatic, but uses fold() which is like a reduce and hence calls an
+     * explicit function in each iteration (in this case its a string append)
+     */
+    //input.chars().rev().fold(String::with_capacity(input.len()), |mut string, c| {
+    //    string.push(c);
+    //    string
+    //    })
+
+    /*
+     * Collect is an implicit version of the above fold, so should be more efficient??
+     */
+    
+     input.chars().rev().collect()
 
     
 }
