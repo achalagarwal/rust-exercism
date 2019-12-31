6a933a1a5e2d1ed8bcf9fe354cfb9f7cbe836c9c
Achal Agarwal <achalagarwal.01@gmail.com>
2019-12-31 10:52:31 +0100

    Benchmarking

--- armstrong-numbers/src/main.rs
+++ armstrong-numbers/src/main.rs
@@ -2,6 +2,9 @@ mod lib;
 // use get_number_of_digits;
 
 fn main(){
+
+    let v = (0..10000000).map(|x| lib::is_armstrong_number(x)).filter(|x| *x ==  true).count();
+    print!("{}", v);
     let d = lib::get_number_of_digits(10000);
     assert_eq!(d, 5);
     let d = lib::get_number_of_digits(10001);
