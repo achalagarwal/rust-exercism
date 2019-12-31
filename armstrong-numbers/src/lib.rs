83dcf1adbfa7ba937de6d42045d83360df5e4591
Achal Agarwal <achalagarwal.01@gmail.com>
2019-12-31 10:30:52 +0100

    Final Armstrong, benchmark pending :)

--- armstrong-numbers/src/lib.rs
+++ armstrong-numbers/src/lib.rs
@@ -1,32 +1,12 @@
 use std::u32;
-use std::convert::TryInto;
 
 pub fn is_armstrong_number(num: u32) -> bool {
-
-    // let digits = get_number_of_digits(num);
-
-    // let sum:u32 = num.to_string().chars().map(|d| d.to_digit(10).unwrap()).map(|n|n.pow(digits)).sum();;
-    let string = num.to_string();
-    let sum:u32 = string.chars().map(|d| d.to_digit(10).unwrap()).map(|n|n.pow((string.len()).try_into().unwrap())).sum();
-    // let iter2 = iter.chars().map(|d| d.to_digit(10).unwrap());
-    // // why is this still at iterator of characters?
-    // let sum:u32 = iter2.map(|n|n.pow(digits)).sum();
-    // print!("{}", sum);
-    return sum == num
-
-    // unimplemented!("true if {} is an armstrong number", num)
-    // let mut 
+    let digits = get_number_of_digits(num);
+    (0..digits).fold((num,0), |acc,_| ((acc.0)/10,acc.1 + ((acc.0 % 10) as u32).pow(digits))).1 == num
 }
 
 pub fn get_number_of_digits(num: u32) -> u32 {
-
-    // there are quite a few ways to determine the number of digits 
-    // iterate while dividing by 10
-    // converting to string and returning len
-    // using log base 10 
     let num_f = num as f32;
-
     (num_f.log10()+1.0) as u32
-    
 }
 
