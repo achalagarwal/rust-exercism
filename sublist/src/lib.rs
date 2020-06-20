5598f79ffc3912c08e05fecf948144e56d33989c
Achal Agarwal <achalagarwal.01@gmail.com>
2020-06-20 15:18:21 -0700

    Cleaned

--- sublist/src/lib.rs
+++ sublist/src/lib.rs
@@ -23,14 +23,8 @@ pub fn sublist<T: PartialEq>(_first_list: &[T], _second_list: &[T]) -> Compariso
         }
     }
 
-    // unimplemented!("Determine if the first list is equal to, sublist of, superlist of or unequal to the second list.");
 }
 pub fn contained<T: PartialEq>(_smaller_list: &[T], _bigger_list: &[T]) -> Comparison {
-
-    // let it = _bigger_list.iter();
-    // it.fold(0,|acc, ele| if acc == 1 {1} else _smaller_list.len() {acc} else if _smaller_list[acc] == ele { acc+1} else {0}
-
-    //     acc+1} chars().nth(index).unwrap() == x {(acc, index+1)} else {(acc+1,index+1)}).0)
 
     for (i, _ch) in _bigger_list.iter().enumerate() {
         if i + _smaller_list.len() > _bigger_list.len(){
@@ -54,8 +48,6 @@ pub fn contained<T: PartialEq>(_smaller_list: &[T], _bigger_list: &[T]) -> Compa
 }
 pub fn equal<T: PartialEq>(list_a: &[T], list_b: &[T]) -> Comparison {
 
-    // let it = _bigger_list.iter();
-    // it.fold(0,|acc, ele| if _smaller_list[acc] == ele {acc+1} chars().nth(index).unwrap() == x {(acc, index+1)} else {(acc+1,index+1)}).0)
     if contained(list_a, list_b) == Comparison::Sublist{
         return Comparison::Equal
     }
